0a2d34bb67b7aa97d3ad0af6ebbfc5f2c3b320e9
Olivier Audard <audard@gmail.com>
2016-05-01 15:33:23 +0200

    Setup CycleJS+Webpack

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+ entry: './src/main.js',
+ output: {
+   filename: 'dist/bundle.js'
+ },
+ module: {
+
+   loaders: [
+     {
+       test: /\.js$/,
+       exclude: /node_modules/,
+       loader: 'babel-loader'
+     }
+   ]
+ },
+ resolve: {
+   extensions: ['', '.js', '.es6']
+ }
+}
